75171858ea3101bbba1b7eb093b42ecb80701017
dowsam <l.xue.nong@gmail.com>
2012-09-05 14:00:02 +0800

    增加打包的方式

--- src/main/java/cn/com/rebirth/search/server/RebirthSearchServerVersion.java
+++ src/main/java/cn/com/rebirth/search/server/RebirthSearchServerVersion.java
@@ -1,26 +1,25 @@
 /*
- * Copyright (c) 2005-2012 www.summall.com.cn All rights reserved
- * Info:summall-search-server VersionImpl.java 2012-3-30 9:08:36 l.xue.nong$$
+ * Copyright (c) 2005-2012 www.china-cti.com All rights reserved
+ * Info:rebirth-search-server RebirthSearchServerVersion.java 2012-7-19 13:14:18 l.xue.nong$$
  */
 package cn.com.rebirth.search.server;
 
+import cn.com.rebirth.commons.AbstractVersion;
 import cn.com.rebirth.commons.Version;
 
 /**
- * The Class VersionImpl.
+ * The Class RebirthSearchServerVersion.
  *
  * @author l.xue.nong
  */
-public class RebirthSearchServerVersion implements Version {
+public class RebirthSearchServerVersion extends AbstractVersion implements Version {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -3140641643166753463L;
 
-	@Override
-	public String getModuleVersion() {
-		return "0.0.1.RC1-SNAPSHOT";
-	}
-
+	/* (non-Javadoc)
+	 * @see cn.com.rebirth.commons.Version#getModuleName()
+	 */
 	@Override
 	public String getModuleName() {
 		return "rebirth-search-server";
